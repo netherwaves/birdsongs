b775851e3a1df0a75f9220a297f4eea7887b461e
netherwaves <avery@netherwaves.com>
2022-07-06 23:00:59 -0400

    add LCD1602 support; add (nonfunctioning) .wav sampler

---
+++ src/lcd_1602.h
@@ -0,0 +1,80 @@
+#pragma once
+#ifndef DSY_LCD_1602_H
+#define DSY_LCD_1602_H
+
+#include "daisy_core.h"
+#include "per/gpio.h"
+
+
+/**
+   @brief Device Driver for 16x2 LCD panel. \n 
+   HD44780 with 4 data lines. \n
+   Example product: https://www.adafruit.com/product/181
+   @author StaffanMelin
+   @date March 2021
+   @ingroup device
+*/
+namespace daisy {
+  class Lcd1602
+  {
+    public:
+      Lcd1602() {}
+      ~Lcd1602() {}
+
+      struct Config
+      {
+          bool         cursor_on;
+          bool         cursor_blink;
+          dsy_gpio_pin rs, en, d4, d5, d6, d7;
+      };
+
+      /** 
+      Initializes the LCD.
+      * \param config is a struct that sets cursor on/off, cursor blink on/off and the dsy_gpio_pin's that connects to the LCD.
+      */
+      void Init(const Config &config);
+
+      /** 
+      Prints a string on the LCD.
+      * \param string is a C-formatted string to print.
+      */
+      void Print(const char *string);
+
+      /** 
+      Prints an integer value on the LCD.
+      * \param number is an integer to print.
+      */
+      void PrintInt(int number);
+
+      /** 
+      Moves the cursor of the LCD (the place to print the next value).
+      * \param row is the row number (0 or 1).
+      * \param col is the column number (0 to 15).
+      */
+      void SetCursor(uint8_t row, uint8_t col);
+
+      /** 
+      Clears the contents of the LCD.
+      */
+      void Clear();
+
+      void Home();
+
+    private:
+      bool     cursor_on;
+      bool     cursor_blink;
+      dsy_gpio lcd_pin_rs;
+      dsy_gpio lcd_pin_en;
+      dsy_gpio lcd_data_pin[4]; // D4-D7
+
+      void Set4BitMode();
+
+      void WriteData(uint8_t);
+      void WriteCommand(uint8_t);
+      void Write(uint8_t, uint8_t);
+  };
+
+}
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,46 @@
 #include <daisy_seed.h>
 
 // stuff
-#include "pitch_detector.h"
-#include "in_to_out.h"
+// #include "pitch_detector.h"
+// #include "in_to_out.h"
+#include "wav_reader.h"
+
+#include "lcd_1602.h"
+
+// #include "Lcd595.h"
 
 // namespaces
 using namespace daisy;
-DaisySeed hardware;
+
+DaisySeed hw;
 
 
 int main(void) {
-    hardware.Configure();
-    hardware.Init();
+    // initialize hardware
+    hw.Configure();
+    hw.Init();
 
-    hardware.StartAudio(InToOut);
+    // config LCD screen
+    Lcd1602::Config lcd_config;
+    // cursor
+    lcd_config.cursor_blink = false;
+    lcd_config.cursor_on = false;
+    // pins
+    lcd_config.rs = hw.GetPin(28);
+    lcd_config.en = hw.GetPin(27);
+    lcd_config.d4 = hw.GetPin(26);
+    lcd_config.d5 = hw.GetPin(25);
+    lcd_config.d6 = hw.GetPin(24);
+    lcd_config.d7 = hw.GetPin(23);
+
+    Lcd1602 lcd;
+    lcd.Init(lcd_config);
+
+    lcd.SetCursor(0, 2);
+    lcd.Print("TALK TO ME");
+
+    lcd.SetCursor(1, 3);
+    lcd.Print("--    --");
+
+    //setup_sampler(hw);
 }
